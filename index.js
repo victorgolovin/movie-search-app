6ce6e9a977ae03ba86b033bc7011c6484a179568
Viktor Golovin <rinoally@gmail.com>
2023-07-10 21:08:55 +0300

    start JS

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-fetch("https://jsonplaceholder.typicode.com/todos", {
-    method: "POST",
-    body: JSON.stringify({
-        title: 'новая задача',
-        completed: false
-    }),
-    headers: {
-        'content-type': 'application/json'
-    }
-})
-    .then(Response => Response.json())
-    .then(json => console.log(json))
+const movieSearchInput = document.getElementById("movie-search-app-input")
+const movieSearchButton = document.getElementById("movie-search-app-button")
 
-
-//--------------------------
-
-const params = new URLSearchParams(location.search);
-
-const id = params.get('id');
-
-console.log(id);
-
-fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    .then(Response => Response.json())
-    .then(json => console.log(json))
