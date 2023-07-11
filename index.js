261d9e335f6248919ba640fa0fa4d07a60dd79fb
Viktor Golovin <rinoally@gmail.com>
2023-07-11 20:06:52 +0300

    add commit

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// const movies = [];
+
 const movieSearchInput = document.getElementById("movie-search-app-input")
 const movieSearchButton = document.getElementById("movie-search-app-button")
 
+const movieHandler = () => {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8a6b6056")
+        .then(Response => Response.json())
+        .then(json => {
+            const movieSearch = json;
+
+            document.getElementById("movie-search-list").innerHTML = `
+            <p>
+            ${movieSearch}
+            </p>
+                `;
+        });
+};
+
+const addMovie = () => {
+
+};
+
+
+
+movieSearchButton.addEventListener("click", movieHandler);
