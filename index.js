dfacabee4064142422276207ae421fec72c098cb
Viktor Golovin <rinoally@gmail.com>
2023-07-15 19:55:45 +0300

    add renderMoviesList

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+const MOVIE_SEARCH_TEXT_NOT_FOUND = 'Films not found';
+const MOVIE_SEARCH_TEXT_NOT_FOUND_FILMS = "movie-search-output";
 
 
 const movieSearchInputNode = document.getElementById("movie-search-app-input")
@@ -13,8 +14,8 @@ const getMovieFromUser = () => {
     const searchMovie = (movieSearchInputNode.value).trim();
     if (!searchMovie) {
         clearSearchInput(movieSearchInputNode);
-        movieSearchOutputNode.classList.add(TEXT_NOT_FOUND_FILMS_CLASSNAME);
-      return movieSearchOutputNode.innerText = TEXT_NOT_FOUND_FILMS;
+        movieSearchOutputNode.classList.add(MOVIE_SEARCH_TEXT_NOT_FOUND_FILMS);
+      return movieSearchOutputNode.innerText = MOVIE_SEARCH_TEXT_NOT_FOUND;
     } 
     
     if (searchMovie) {
@@ -27,6 +28,62 @@ const getMovieFromUser = () => {
 const clearSearchInput = (element) => {
     element.value = '';
 };
+
+const movieSearchGetMovies = (searchMovie) => {
+    fetch(`https://www.omdbapi.com/?s=${searchMovie}&apikey=8a6b6056`)
+      .then(response => response.json())
+      .then((data) => {
+        if (data.Response) {
+          renderMoviesList(data);
+        }
+        if  (!data.Response) {
+          outputNode.classList.add(MOVIE_SEARCH_TEXT_NOT_FOUND_FILMS);
+          return outputNode.innerText = MOVIE_SEARCH_TEXT_NOT_FOUND;
+        }
+      })
+  };
+
+const renderMoviesList = (data) => {
+    movieSearchListNode.innerHTML = '';
+    data.Search.forEach((movie) => {
+  
+      const movieFeatures = document.createElement('li');
+      movieFeatures.classList.add('movie-item');
+      movieFeatures.setAttribute('id', `${movie.imdbID} `);
+  
+      if (movie.Poster !== 'N/A') {
+        moviePoster = movie.Poster;
+      } 
+      if (movie.Poster == 'N/A') {
+        moviePoster = 'resources/movie-not-found.webp'
+      }
+  
+      movieFeatures.innerHTML = `
+      
+      <img
+      class="movie-features_img"
+      src="${moviePoster}"
+      alt="Превью фильма"
+    />
+      <div class="movie-features-wrapper">
+        <h2 class="movie-features-title">${movie.Title}</h2>
+        <p class="movie-features-year">${movie.Year}</p>
+        <p class="movie-features-category">${movie.Type}</p>
+      </div>
+     
+    `;
+  
+    movieSearchListNode.appendChild(movieFeatures);
+    });
+    
+    searchMovieLoad();
+  }
+
+const searchMovieLoad = () => {
+
+}
+
+
 
 
 
